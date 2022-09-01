6be884b3363225f7bf3371ce15aa66b4339b6a4f
Arina Galieva <a.galieva@just-ai.com>
2022-09-01 23:32:22 +0300

    New scripts

--- scripts.js
+++ scripts.js
@@ -1,86 +1,81 @@
-document.querySelectorAll('.main__devices').forEach(node => {
-    const tabsContainer = node.querySelector('.section__tabs');
-    const select = node.querySelector('.section__select');
-    const tabs = tabsContainer.querySelectorAll('.section__tab');
+function selectTab (tabsContainer, newId) {
+    const activeTab = tabsContainer.querySelector('.section__tab_active');
 
-    const list = [];
-    let selected = '';
-    tabs.forEach((node) => {
-        list.push(node.dataset.id);
-        if (node.classList.contains('.section__tab_active')) {
-            selected = node.dataset.id;
-        }
+    const newTab = tabsContainer.querySelector(`.section__tab[data-id=${newId}]`);
+    const newPanel = tabsContainer.parentElement.parentElement.querySelector(`.section__panel[data-id=${newId}]`);
+    const oldPanel = tabsContainer.parentElement.parentElement.querySelector('.section__panel:not(.section__panel_hidden)');
+
+    activeTab.classList.remove('section__tab_active');
+    activeTab.setAttribute('aria-selected', 'false');
+    activeTab.removeAttribute('tabindex');
+    newTab.classList.add('section__tab_active');
+    newTab.setAttribute('aria-selected', 'true');
+    newTab.setAttribute('tabindex', '0');
+    newTab.focus({
+        preventScroll: true
     });
 
-    function selectTab(newId) {
-        selected = newId;
-        const newTab = tabsContainer.querySelector(`.section__tab[data-id=${newId}]`);
-        const oldTab = tabsContainer.querySelector('.section__tab_active');
-        const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
-        const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+    oldPanel.classList.add('section__panel_hidden');
+    oldPanel.setAttribute('aria-hidden', 'true');
+    newPanel.classList.remove('section__panel_hidden');
+    newPanel.setAttribute('aria-hidden', 'false');
+}
 
-        oldTab.classList.remove('section__tab_active');
-        oldTab.setAttribute('aria-selected', 'false');
-        oldTab.removeAttribute('tabindex');
-        newTab.classList.add('section__tab_active');
-        newTab.setAttribute('aria-selected', 'true');
-        newTab.setAttribute('tabindex', '0');
-        newTab.focus({
-            preventScroll: true
-        });
-
-        oldPanel.classList.add('section__panel_hidden');
-        oldPanel.setAttribute('aria-hidden', 'true');
-        newPanel.classList.remove('section__panel_hidden');
-        newPanel.setAttribute('aria-hidden', 'false');
+function handleTabClick(event) {
+    if (!event.target.classList.contains('section__tab')) {
+        return;
     }
 
-    select.addEventListener('input', () => {
-        selectTab(select.value);
-    });
+    const tabNode = event.target;
+    const tabsContainer = event.currentTarget;
 
-    tabsContainer.addEventListener('click', (event) => {
-        if (event.target.classList.contains('section__tab')) {
-            selectTab(event.target.dataset.id);
+    const newId = tabNode.dataset.id;
+
+    selectTab(tabsContainer, newId);
+}
+
+function handleTabKeyDown(event) {
+    if (!event.target.classList.contains('section__tab') || event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+        return;
+    }
+
+    const activeTab = event.currentTarget.querySelector('.section__tab_active');
+    const tabs = Array.from(event.currentTarget.querySelectorAll('.section__tab'));
+    let index = tabs.indexOf(activeTab);
+
+    if (event.keyCode === 37) {
+        if (--index < 0){
+            index = tabs.length - 1;
         }
-    })
-
-    tabsContainer.addEventListener('keydown', (event) => {
-        if (!event.target.classList.contains('section__tab') || event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
-            return;
-        }
-
-        let index = list.indexOf(selected);
-        if (event.keyCode === 37) {
-            if (--index < 0){
-                index = list.length - 1;
-            }
-        } else if (event.keyCode === 39) {
-            if (++index >= list.length) {
-                index = 0;
-            }
-        } else if (event.keyCode === 36) {
+    } else if (event.keyCode === 39) {
+        if (++index >= tabs.length) {
             index = 0;
-        } else if (event.keyCode === 35) {
-            index = list.length - 1;
-        } else {
-            return;
         }
+    } else if (event.keyCode === 36) {
+        index = 0;
+    } else if (event.keyCode === 35) {
+        index = tabs.length - 1;
+    } else {
+        return;
+    }
 
-        selectTab(list[index]);
-    })
-});
-document.querySelectorAll('.header__menu').forEach(node => {
-    let expanded = false;
-    const menuTextNode = node.querySelector('.header__menu-text');
+    selectTab(event.currentTarget, tabs[index].dataset.id);
+}
 
-    node.addEventListener('click', () => {
-        expanded = !expanded;
-        node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
-        menuTextNode.textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
-        links.classList.toggle('header__links_opened', expanded);
-        links.classList.add('header__links-toggled');
-    });
-});
+function onBurgerClick(event) {
+    const headerMenu = event.currentTarget;
+    const menuTextNode = headerMenu.querySelector('.header__menu-text');
+    const links = headerMenu.parentElement.querySelector('.header__links');
 
-const links = document.querySelector('.header__links');
+    const isExpandedNow = links.classList.contains("header__links_opened");
+    const willExpand = !isExpandedNow;
+
+    headerMenu.setAttribute('aria-expanded', String(willExpand));
+    menuTextNode.textContent = willExpand ? 'Закрыть меню' : 'Открыть меню';
+    if (isExpandedNow) {
+        links.classList.remove('header__links_opened');
+    } else {
+        links.classList.add('header__links_opened');
+    }
+    links.classList.add('header__links-toggled');
+}
